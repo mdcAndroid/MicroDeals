566e501356c4bf0ead9303fdc813ee0ab10ee9ca
Patrick H. <mdc.patrick.helmus@gmail.com>
2013-03-24 12:58:30 -0400

    Splash Screen Commit
    Pivotal ID: 46058239

---
+++ src/com/example/microdeal/Splash.java
@@ -0,0 +1,52 @@
+package com.example.microdeal;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_LEFT_ICON);
+        setContentView(R.layout.activity_splash);
+        setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,R.drawable.microdeallaunchicon );
+        ImageView imgInterpolate=(ImageView)findViewById(R.id.imageView1);
+        imgInterpolate.startAnimation(AnimationUtils.loadAnimation(this,R.anim.interpolate));
+
+       TimerTask task= new TimerTask() {
+       	public void run() {
+       		startActivity (new Intent(Splash.this, Main.class));
+       	}
+       
+       };
+       Timer opening=new Timer();
+       opening.schedule(task,5000);
+       
+       
+   }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
+    
+}
